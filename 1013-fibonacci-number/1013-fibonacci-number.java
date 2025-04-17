b3561d7e1777bac419961b00d8c7d5c062890afe
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2025-04-18 00:28:52 +0530

    Time: 0 ms (100%), Space: 40.5 MB (49.14%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,15 +1,25 @@
 class Solution {
+    public int memo(int n,ArrayList<Integer> m){
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (m.get(n) != -1) {
+            return m.get(n);
+        }
+        int fibNMinus1 = memo(n - 1, m);
+        int fibNMinus2 = memo(n - 2, m);
+        m.set(n, fibNMinus1 + fibNMinus2);
+        return m.get(n);
+    }
     public int fib(int n) {
         if(n==0){
             return 0;
         }
-        ArrayList<Integer> m= new ArrayList<>(Collections.nCopies(n+1,0));
-        m.set(0,0);
-        m.set(1,1);
-        for(int i=2;i<=n;i++){
-            m.set(i,(m.get(i-2)+m.get(i-1)));
-        }
-        return m.get(n);
+        ArrayList<Integer> m= new ArrayList<>(Collections.nCopies(n+1,-1));
+      return memo(n,m);
         
     }
 }
