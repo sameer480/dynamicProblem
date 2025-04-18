049bc90f28630ff6f567e3fe1dd231b35bd9ae6d
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2025-04-18 17:38:03 +0530

    Time: 0 ms (100%), Space: 40.8 MB (26.58%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int [][] mat= new int [m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(mat[i],1);
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                mat[i][j]=mat[i-1][j]+mat[i][j-1];
+            }
+          
+        }
+          return mat[m-1][n-1];
+    }
+}
