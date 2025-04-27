e87316463b51f8a10f2f1ff1f937a8355aa737f8
Sameer Jawed <90494125+sameer480@users.noreply.github.com>
2025-04-28 01:03:59 +0530

    Time: 15 ms (15.24%), Space: 59.6 MB (9.44%) - LeetHub

---
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
@@ -0,0 +1,25 @@
+class Solution {
+    int solve(int low, int high, int zero, int one, int i, long[] dp) {
+        long mod = 1_000_000_007;
+        
+        if (i > high) {
+            return 0;
+        }
+        
+        if (dp[i] != -1) {
+            return (int) dp[i];
+        }
+        
+        dp[i] = i >= low ? 1 : 0;
+        dp[i] = (dp[i] + solve(low, high, zero, one, i + zero, dp)) % mod;
+        dp[i] = (dp[i] + solve(low, high, zero, one, i + one, dp)) % mod;
+        
+        return (int) dp[i];
+    }
+    
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        long[] dp = new long[high + 1];
+        Arrays.fill(dp, -1L);
+        return solve(low, high, zero, one, 0, dp);
+    }
+}
